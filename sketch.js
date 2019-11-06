d3d820a778dd91721ef7ea0ad66265c953798330
BrettStapleton <brett.i.stapleton@gmail.com>
2019-11-06 17:09:15 -0600

    Created 16x16 grid

---
+++ sketch.js
@@ -0,0 +1,8 @@
+const container = document.querySelector('#container');
+
+for (let i=0; i < (16*16); i++){
+    const box = document.createElement('div');
+    box.classList.add('square');
+    container.appendChild(box);
+}
+
