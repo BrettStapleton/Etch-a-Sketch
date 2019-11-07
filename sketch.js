825c09aa5765d363f85df7bec42d36c3c69c0a52
BrettStapleton <brett.i.stapleton@gmail.com>
2019-11-07 03:43:26 -0600

    Add color and gradient

--- sketch.js
+++ sketch.js
@@ -1,10 +1,31 @@
 const container = document.querySelector('#container');
+const button = document.querySelector('#clear-btn');
+button.addEventListener('click', clearScreen);
 
-for (let i=0; i < (16*16); i++){
-    const square = document.createElement('div');
-    square.classList.add('square');
-    square.addEventListener('mouseover', function(e){
-        e.target.style.backgroundColor = 'red';
-    });
-    container.appendChild(square);
+function createGrid(squaresPerSide) {
+    container.style.gridTemplateColumns = `repeat(${squaresPerSide}, 40px)`;
+    container.style.gridTemplateRows = `repeat(${squaresPerSide}, 40px)`;
+    
+    for (let i=0; i < (squaresPerSide * squaresPerSide); i++){
+        const square = document.createElement('div');
+        square.classList.add('square');
+        square.setAttribute("lightness", 50);
+        square.addEventListener('mouseover', function(e){
+            currentLightness = e.target.getAttribute("lightness");
+            e.target.style.backgroundColor = `hsl(${Math.floor(Math.random()*361)}, 100%, ${currentLightness}%)`;
+            e.target.setAttribute("lightness", currentLightness-5);
+        });
+        container.appendChild(square);
+    }
 }
+
+function clearScreen() {
+    const squares = document.querySelectorAll('.square');
+    squares.forEach((square) => {
+        square.remove();
+    });
+    const userSquares = prompt("Enter number of squares per side");
+    createGrid(userSquares);
+}
+
+createGrid(16);
